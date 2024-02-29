f83dda64f63b28b3cd2e31804b526e741ae5446b
Romário Lima <devromariolima@gmail.com>
2024-02-29 00:52:00 -0300

    otimização do fetch

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
 
-const apiUrl = 'https://api.adviceslip.com/advice';
-let result ;
-fetch (apiUrl)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`Ocorreu um erro: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then(data => { result = data;
-    console.log('Dados recebidos:', data);
-  })
-  .catch(error => {
-    console.error('Ocorreu um erro ao buscar os dados:', error);
-
-  });
-
-
-  function ExibirDados (){
+async function ExibirDados (){
+    const apiUrl = 'https://api.adviceslip.com/advice';
+    let result ;
+    await fetch (apiUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Ocorreu um erro: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => { result = data;
+        console.log('Dados recebidos:', data);
+      })
+      .catch(error => {
+        console.error('Ocorreu um erro ao buscar os dados:', error);
+    
+      });
+    
     document.getElementById('advice').innerHTML = result.slip.advice;
 
-  
-  }
+  } 
